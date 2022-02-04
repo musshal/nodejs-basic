d4bf83261c6da4e132b898b49407415501ac9c25
Musthafa Faishal <musthafafaishal@gmail.com>
2022-02-04 14:44:30 +0700

    First commit

---
+++ filesystem/index.js
@@ -0,0 +1,16 @@
+// TODO: tampilkan teks pada notes.txt pada console.
+
+const fs = require('fs');
+const path = require('path');
+
+const fileReadCallBack = (error, data) => {
+  if (error) {
+    console.log('Gagal membaca berkas');
+
+    return;
+  }
+
+  console.log(data);
+};
+
+fs.readFile(path.resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallBack);
---
+++ stream/index.js
@@ -0,0 +1,24 @@
+/**
+ * TODO:
+ * Buatlah program untuk membaca teks input.txt dan menuliskannya ulang pada berkas output.txt
+ * menggunakan teknik readable stream dan writable stream.
+ */
+
+const fs = require('fs');
+
+const readableStream = fs.createReadStream('./stream/input.txt', {
+  highWaterMark: 15,
+});
+const writableStream = fs.createWriteStream('./stream/output.txt');
+
+readableStream.on('readable', () => {
+  try {
+    writableStream.write(`[${readableStream.read()}]\n`);
+  } catch (error) {
+    // catch the error when the chunk cannot be read.
+  }
+});
+
+readableStream.on('end', () => {
+  console.log('Done');
+});
